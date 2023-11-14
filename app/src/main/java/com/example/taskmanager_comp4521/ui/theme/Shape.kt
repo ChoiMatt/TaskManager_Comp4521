0422ad6bcea1fcd301ff90e2545dd8d1977552d2
mattc <mattchoi722@gail.com>
2023-11-14 09:05:18 +0800

    Modify the theme color and font for the project

---
+++ app/src/main/java/com/example/taskmanager_comp4521/ui/theme/Shape.kt
@@ -0,0 +1,10 @@
+package com.example.taskmanager_comp4521.ui.theme
+
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Shapes
+import androidx.compose.ui.unit.dp
+
+val Shapes = Shapes(
+    small = RoundedCornerShape(25.dp),
+    medium = RoundedCornerShape(bottomStart = 16.dp, topEnd = 16.dp)
+)
--- app/src/main/java/com/example/taskmanager_comp4521/ui/theme/Type.kt
+++ app/src/main/java/com/example/taskmanager_comp4521/ui/theme/Type.kt
@@ -2,33 +2,77 @@ package com.example.taskmanager_comp4521.ui.theme
 
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import com.example.taskmanager_comp4521.R
+
+val Nunito = FontFamily(
+    listOf(
+        Font(resId = R.font.nunito_italic, weight = FontWeight.Light),
+        Font(resId = R.font.nunito_regular, weight = FontWeight.Normal),
+        Font(resId = R.font.nunito_extra_bold, weight = FontWeight.ExtraBold)
+    )
+)
+
+val Poppins = FontFamily(
+    listOf(
+        Font(resId = R.font.poppins_light, weight = FontWeight.Normal),
+        Font(resId = R.font.poppins_semi_bold, weight = FontWeight.Bold),
+    )
+)
 
 // Set of Material typography styles to start with
+//val Typography = Typography(
+//    bodyLarge = TextStyle(
+//        fontFamily = FontFamily.Default,
+//        fontWeight = FontWeight.Normal,
+//        fontSize = 16.sp,
+//        lineHeight = 24.sp,
+//        letterSpacing = 0.5.sp
+//    )
+//    /* Other default text styles to override
+//    titleLarge = TextStyle(
+//        fontFamily = FontFamily.Default,
+//        fontWeight = FontWeight.Normal,
+//        fontSize = 22.sp,
+//        lineHeight = 28.sp,
+//        letterSpacing = 0.sp
+//    ),
+//    labelSmall = TextStyle(
+//        fontFamily = FontFamily.Default,
+//        fontWeight = FontWeight.Medium,
+//        fontSize = 11.sp,
+//        lineHeight = 16.sp,
+//        letterSpacing = 0.5.sp
+//    )
+//    */
+//)
 val Typography = Typography(
-    bodyLarge = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 16.sp,
-        lineHeight = 24.sp,
-        letterSpacing = 0.5.sp
-    )
-    /* Other default text styles to override
-    titleLarge = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 22.sp,
-        lineHeight = 28.sp,
-        letterSpacing = 0.sp
+    headlineLarge = TextStyle(
+        fontFamily = Nunito,
+        fontWeight = FontWeight.ExtraBold,
+        fontSize = 32.sp
     ),
-    labelSmall = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Medium,
-        fontSize = 11.sp,
-        lineHeight = 16.sp,
-        letterSpacing = 0.5.sp
+    headlineMedium = TextStyle(
+        fontFamily = Poppins,
+        fontWeight = FontWeight.Bold,
+        fontSize = 18.sp
+    ),
+    titleMedium = TextStyle(
+        fontFamily = Nunito,
+        fontWeight = FontWeight.Normal,
+        fontSize = 14.sp
+    ),
+    labelMedium = TextStyle(
+        fontFamily = Poppins,
+        fontWeight = FontWeight.Normal,
+        fontSize = 14.sp
+    ),
+    labelSmall =  TextStyle(
+        fontFamily = Nunito,
+        fontWeight = FontWeight.Light,
+        fontSize = 14.sp
     )
-    */
 )
