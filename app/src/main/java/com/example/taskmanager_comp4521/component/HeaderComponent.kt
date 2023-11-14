8e069bc5305816a564d7ef2e8ff95fa1c12c0501
mattc <mattchoi722@gail.com>
2023-11-14 09:15:00 +0800

    add top app bar for the project

---
+++ app/src/main/java/com/example/taskmanager_comp4521/component/HeaderComponent.kt
@@ -0,0 +1,47 @@
+package com.example.taskmanager_comp4521.component
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Menu
+import androidx.compose.material.icons.filled.Settings
+import androidx.compose.material3.Icon
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
+import com.example.taskmanager_comp4521.R
+
+@Composable
+fun HeaderComponent(MenuBTNonClick: () -> Unit) {
+    Row(
+        modifier = Modifier.fillMaxWidth(),
+        horizontalArrangement = Arrangement.SpaceBetween,
+        verticalAlignment = Alignment.CenterVertically)
+    {
+        Icon(imageVector = Icons.Filled.Menu,
+            contentDescription = "Menu",
+            modifier = Modifier
+                .padding(end= 16.dp)
+                .clickable(onClick = MenuBTNonClick)
+        )
+        Image(painter = painterResource(id = R.drawable.wonder_woman),
+            contentDescription = "Profile Picture",
+            modifier = Modifier
+                .size(30.dp)
+                .clip(CircleShape)
+        )
+        Icon(imageVector = Icons.Filled.Settings,
+            contentDescription = "Settings",
+            modifier = Modifier.padding(end= 16.dp))
+    }
+}
+
